7703c483ee0adc8aed96704fe6faf1b049b20252
Zakher Masri <zakhermasri@gmail.com>
2021-11-03 07:41:25 +0300

    updated position styling

--- src/components/Loah/Loah.js
+++ src/components/Loah/Loah.js
@@ -7,7 +7,15 @@ export const Loah = (props) => {
     color: props.buttonsColor,
     position: "absolute",
     overflow: "hidden",
-    zIndex: 10,
+    zIndex: 10
+  };
+
+  let tabPanelStyle = {
+    // backgroundColor: props.bgColor,
+    color: props.buttonsColor,
+    position: "absolute",
+    overflow: "hidden",
+    zIndex: 10
   };
   switch (props.direction) {
     case "right":
@@ -16,7 +24,19 @@ export const Loah = (props) => {
         top: 0,
         right: 0,
         height: "100%",
-        width: 60,
+        width: 60
+      };
+      tabPanelStyle = {
+        // height: '100%',
+        ...tabPanelStyle,
+        height: 300,
+        position: "absolute",
+        top: 0,
+        right: 0,
+        zIndex: 9,
+        backgroundColor: "orange",
+        width: 0,
+        overflow: "hidden"
       };
       break;
     case "left":
@@ -25,7 +45,7 @@ export const Loah = (props) => {
         top: 0,
         left: 0,
         width: 60,
-        height: "100%",
+        height: "100%"
       };
       break;
     case "top":
@@ -34,7 +54,7 @@ export const Loah = (props) => {
         top: 0,
         left: 0,
         width: "100%",
-        height: 60,
+        height: 60
       };
       break;
     case "bottom":
@@ -43,7 +63,7 @@ export const Loah = (props) => {
         bottom: 0,
         left: 0,
         height: 60,
-        width: "100%",
+        width: "100%"
       };
       break;
     default:
@@ -52,7 +72,7 @@ export const Loah = (props) => {
         top: 0,
         right: 0,
         height: "100%",
-        width: 60,
+        width: 60
       };
   }
   return (
@@ -66,59 +86,71 @@ export const Loah = (props) => {
           onClick={props.handleExpand}
           id="expandButton"
           style={{
-            backgroundColor: "green",
+            // backgroundColor: 'blue',
             height: 60,
+            width: 60,
             zIndex: 10,
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
-            // outline: "1px solid black"
+            position: "relative",
+            paddingLeft: 5
           }}
         >
           <ExpandIcon />
         </div>
-        {props.buttons?.map((btn) => {
+        {props.buttons?.map((btn, k) => {
           return (
             <div
+              key={k}
               style={{
                 display: "flex",
                 flexDirection: "row",
                 // backgroundColor: "green",
                 height: 60,
                 width: "100%",
-                zIndex: 10,
-                justifyContent: "center",
-                alignItems: "center",
+                zIndex: 10
+                // justifyContent: 'center',
+                // alignItems: 'center'
                 // outline: "1px solid black"
               }}
             >
-              <div
-                style={{
-                  // backgroundColor: "green",
-                  height: 60,
-                  width: 60,
-                  zIndex: 10,
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  // outline: "1px solid black"
-                }}
-              >
-                {btn.icon}
-              </div>
+              {btn.icon ? (
+                <div
+                  style={{
+                    // backgroundColor: "green",
+                    height: 60,
+                    width: 60,
+                    zIndex: 10,
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center"
+                    // outline: "1px solid black"
+                  }}
+                >
+                  {btn.icon}
+                </div>
+              ) : null}
               {props.expended ? (
                 <motion.div
+                  onClick={() => console.log("clicking ", btn.name)}
                   style={{
                     backgroundColor: "yellow",
-                    color: "black",
-                    height: "100%",
+                    // margin: 5
+                    cursor: "pointer",
+                    color: "green",
+                    height: "auto",
+                    width: "100%",
                     display: "flex",
                     alignItems: "center",
+                    padding: 20
+                    // justifyContent:'center'
+                    // textAlign: 'center'
                   }}
                   initial={{ opacity: 0, width: 0 }}
                   animate={
                     props.expended
-                      ? { opacity: 1, width: "100%" }
+                      ? { opacity: 1, width: "90%" }
                       : { opacity: 0, width: 0 }
                   }
                 >
@@ -128,29 +160,35 @@ export const Loah = (props) => {
             </div>
           );
         })}
+
+        {props.expended && props.footer ? (
+          <motion.div
+            style={{
+              bottom: 0,
+              position: "absolute",
+              // backgroundColor: "blue",
+              width: "100%",
+              paddingTop: 10,
+              paddingBottom: 10,
+              textAlign: "center"
+              // color: "white"
+            }}
+            initial={{ opacity: 0, width: 0 }}
+            animate={
+              props.expended
+                ? { opacity: 1, width: "100%" }
+                : { opacity: 0, width: 0 }
+            }
+          >
+            {props.footer}
+          </motion.div>
+        ) : null}
       </motion.div>
       <motion.div
-        initial={{
-          height: "100%",
-          position: "absolute",
-          top: 0,
-          right: 0,
-          zIndex: 9,
-          backgroundColor: "orange",
-          width: 0,
-          overflow: "hidden",
-        }}
-        animate={
-          props.expended
-            ? {
-                width: 300,
-              }
-            : {
-                width: 0,
-              }
-        }
+        initial={{ ...tabPanelStyle }}
+        animate={props.expended ? { width: 300 } : { width: 0 }}
       >
-        {props.expended ? <div>tab content</div> : null}
+        {props.expended && props.content ? <div>tab content</div> : null}
       </motion.div>
     </div>
   );
--- src/stories/Loah.stories.js
+++ src/stories/Loah.stories.js
@@ -1,31 +1,37 @@
-import React, { useState } from "react";
-import { storiesOf } from "@storybook/react";
-import { Loah } from "../components/Loah/Loah";
+import React, { useState } from 'react'
+import { storiesOf } from '@storybook/react'
+import { Loah } from '../components/Loah/Loah'
 
-const stories = storiesOf("App Test", module);
+const stories = storiesOf('Loah', module)
 
-stories.add("App", () => {
-  const [expand, setExpand] = useState(false);
+stories.add('Light', () => {
+  const [expand, setExpand] = useState(false)
 
   const buttons = [
-    {
-      name: "something",
-    },
-    {
-      name: "something",
-    },
-    {
-      name: "something",
-    },
-  ];
+    { name: 'button 1' },
+    { name: 'button 2' },
+    { name: 'button 3' }
+  ]
 
   return (
     <Loah
+      // {...args}
       expended={expand}
       handleExpand={() => setExpand(!expand)}
-      bgColor={"red"}
-      direction={"right"}
+      bgColor={'red'}
+      // direction={"right"}
       buttons={buttons}
     />
-  );
-});
+  )
+})
+
+// export default {
+//   title: "Loah",
+//   component: Loah,
+//   argTypes: {
+//     direction: {
+//       options: ["top", "bottom", "right", "left"],
+//       control: { type: "radio" },
+//     },
+//   },
+// };
