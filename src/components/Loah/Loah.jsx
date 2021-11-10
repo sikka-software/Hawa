87ef109381272109c7a46d4e9d1b1b1cc47f834e
Zakher Masri <zakhermasri@gmail.com>
2021-11-10 04:51:52 +0300

    testing

--- src/components/Loah/Loah.jsx
+++ src/components/Loah/Loah.jsx
@@ -1,28 +1,28 @@
-import { motion } from "framer-motion";
+import { motion } from "framer-motion"
 // import ExpandIcon from "@material-ui/icons/ArrowBackIos";
-import tinycolor from "tinycolor2";
-import { useState } from "react";
+import tinycolor from "tinycolor2"
+import { useState } from "react"
 
 export const Loah = (props) => {
-  const [expand, setExpand] = useState(false);
+  const [expand, setExpand] = useState(false)
   let rootStyle = {
     zIndex: 10,
     fontFamily: "Roboto",
-    fontWeight: 600
-  };
+    fontWeight: 600,
+  }
   let directionStyle = {
     backgroundColor: props.bgColor || tinycolor("lightgrey"),
     color: tinycolor(props.buttonsColor),
     position: "absolute",
     overflow: "hidden",
-    zIndex: 10
-  };
+    zIndex: 10,
+  }
   let tabPanelStyle = {
     color: props.buttonsColor,
     position: "absolute",
     overflow: "hidden",
-    zIndex: 10
-  };
+    zIndex: 10,
+  }
   let expandButtonStyle = {
     height: 60,
     width: 60,
@@ -31,8 +31,8 @@ export const Loah = (props) => {
     justifyContent: "center",
     alignItems: "center",
     position: "relative",
-    paddingLeft: 5
-  };
+    paddingLeft: 5,
+  }
   let itemIconStyle = {
     height: 60,
     width: 60,
@@ -40,8 +40,8 @@ export const Loah = (props) => {
     zIndex: 10,
     display: "flex",
     justifyContent: "center",
-    alignItems: "center"
-  };
+    alignItems: "center",
+  }
   switch (props.direction) {
     case "right":
       directionStyle = {
@@ -49,8 +49,8 @@ export const Loah = (props) => {
         top: 0,
         right: 0,
         height: "100%",
-        width: 60
-      };
+        width: 60,
+      }
       tabPanelStyle = {
         // height: '100%',
         ...tabPanelStyle,
@@ -60,51 +60,51 @@ export const Loah = (props) => {
         right: 0,
         zIndex: 9,
         width: 0,
-        overflow: "hidden"
-      };
-      break;
+        overflow: "hidden",
+      }
+      break
     case "left":
       directionStyle = {
         ...directionStyle,
         top: 0,
         left: 0,
         width: 60,
-        height: "100%"
-      };
-      break;
+        height: "100%",
+      }
+      break
     case "top":
       directionStyle = {
         ...directionStyle,
         top: 0,
         left: 0,
         width: "100%",
-        height: 60
-      };
-      break;
+        height: 60,
+      }
+      break
     case "bottom":
       directionStyle = {
         ...directionStyle,
         bottom: 0,
         left: 0,
         height: 60,
-        width: "100%"
-      };
-      break;
+        width: "100%",
+      }
+      break
     default:
       directionStyle = {
         ...directionStyle,
         top: 0,
         right: 0,
         height: "100%",
-        width: 60
-      };
+        width: 60,
+      }
   }
   return (
     <div style={rootStyle}>
       <motion.div
         initial={directionStyle}
         transition={{
-          stiffness: 100
+          stiffness: 100,
         }}
         animate={expand ? { width: 200 } : { width: 60 }}
       >
@@ -115,89 +115,100 @@ export const Loah = (props) => {
           onClick={() => setExpand(!expand)}
         >
           {/* <ExpandIcon /> */}
-          EXPAND
+          EXP
         </motion.div>
 
         <div
           style={{
-            height: "100%"
+            height: "100%",
             // overflow: expand ? "scroll" : "hidden"
           }}
         >
-          {props.buttons ? props.buttons.map((btn, k) => {
-            return (
-              <motion.div
-                onClick={btn.action}
-                key={k}
-                initial={{
-                  display: "flex",
-                  flexDirection: "row",
-                  height: 60,
-                  width: "100%",
-                  zIndex: 10,
-                  backgroundColor: props.bgColor
-                    ? tinycolor(props.bgColor)
-                    : tinycolor("lightgrey")
-                }}
-                animate={
-                  props.activeItem === btn.slug
-                    ? { backgroundColor: tinycolor(props.bgColor).lighten(60) }
-                    : {}
-                }
-                whileHover={
-                  props.activeItem === btn.slug
-                    ? {
-                        backgroundColor: tinycolor(props.bgColor).lighten(60)
-                      }
-                    : {
-                        backgroundColor: tinycolor(props.bgColor).lighten(75)
-                      }
-                }
-              >
-                {btn.icon ? (
+          {props.buttons
+            ? props.buttons.map((btn, k) => {
+                return (
                   <motion.div
-                    style={itemIconStyle}
+                    onClick={btn.action}
+                    key={k}
                     initial={{
-                      height: 60,
-                      width: 60
-                    }}
-                    transition={{
-                      type: "spring",
-                      stiffness: 100
-                    }}
-                    animate={{ width: 60 }}
-                  >
-                    {btn.icon}
-                  </motion.div>
-                ) : null}
-                {expand ? (
-                  <motion.div
-                    style={{
-                      cursor: "pointer",
-                      height: "auto",
                       display: "flex",
-                      alignItems: "center",
-                      padding: 20,
-                      color: tinycolor(props.textColor) || tinycolor("black")
+                      flexDirection: "row",
+                      height: 60,
+                      width: "100%",
+                      zIndex: 10,
+                      backgroundColor: props.bgColor
+                        ? tinycolor(props.bgColor)
+                        : tinycolor("lightgrey"),
                     }}
-                    transition={{ delay: k * 0.05, bounce: 0 }}
-                    initial={{ opacity: 0, x: 100, padding: 20 }}
                     animate={
-                      expand
+                      props.activeItem === btn.slug
                         ? {
-                            opacity: 1,
-                            x: 0,
-                            padding: btn.icon ? 0 : 20
+                            backgroundColor: tinycolor(props.bgColor).lighten(
+                              60
+                            ),
                           }
-                        : { opacity: 0 }
+                        : {}
+                    }
+                    whileHover={
+                      props.activeItem === btn.slug
+                        ? {
+                            backgroundColor: tinycolor(props.bgColor).lighten(
+                              60
+                            ),
+                          }
+                        : {
+                            backgroundColor: tinycolor(props.bgColor).lighten(
+                              75
+                            ),
+                          }
                     }
                   >
-                    {btn.name}
+                    {btn.icon ? (
+                      <motion.div
+                        style={itemIconStyle}
+                        initial={{
+                          height: 60,
+                          width: 60,
+                        }}
+                        transition={{
+                          type: "spring",
+                          stiffness: 100,
+                        }}
+                        animate={{ width: 60 }}
+                      >
+                        {btn.icon}
+                      </motion.div>
+                    ) : null}
+                    {expand ? (
+                      <motion.div
+                        style={{
+                          cursor: "pointer",
+                          height: "auto",
+                          display: "flex",
+                          alignItems: "center",
+                          padding: 20,
+                          color:
+                            tinycolor(props.textColor) || tinycolor("black"),
+                        }}
+                        transition={{ delay: k * 0.05, bounce: 0 }}
+                        initial={{ opacity: 0, x: 100, padding: 20 }}
+                        animate={
+                          expand
+                            ? {
+                                opacity: 1,
+                                x: 0,
+                                padding: btn.icon ? 0 : 20,
+                              }
+                            : { opacity: 0 }
+                        }
+                      >
+                        {btn.name}
+                      </motion.div>
+                    ) : null}
                   </motion.div>
-                ) : null}
-              </motion.div>
-            )
-          }) : null}
+                )
+              })
+            : null}
           {/* {props.buttons?.map((btn, k) => {
             return (
               <motion.div
@@ -282,7 +293,7 @@ export const Loah = (props) => {
               width: "100%",
               paddingTop: 10,
               paddingBottom: 10,
-              textAlign: "center"
+              textAlign: "center",
             }}
             initial={{ opacity: 0, width: 0 }}
             animate={
@@ -300,5 +311,5 @@ export const Loah = (props) => {
         {expand && props.content ? <div>tab content</div> : null}
       </motion.div>
     </div>
-  );
-};
+  )
+}
