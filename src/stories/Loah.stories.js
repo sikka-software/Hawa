f8e2d6a5f7267bdda6687fc6e99f4675eb1c26fa
Zakher Masri <zakhermasri@gmail.com>
2021-11-10 04:30:00 +0300

    removed material ui from story

--- src/stories/Loah.stories.js
+++ src/stories/Loah.stories.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import { storiesOf } from "@storybook/react";
 import { Loah } from "../components/Loah/Loah";
-import AddAlertIcon from "@material-ui/icons/AddAlert";
-import AllInbox from "@material-ui/icons/AllInbox";
-import Assignment from "@material-ui/icons/Assignment";
-import Assessment from "@material-ui/icons/Assessment";
-import Ballot from "@material-ui/icons/Ballot";
-import Class from "@material-ui/icons/Class";
-import Bolt from "@material-ui/icons/Dvr";
+// import AddAlertIcon from "@material-ui/icons/AddAlert";
+// import AllInbox from "@material-ui/icons/AllInbox";
+// import Assignment from "@material-ui/icons/Assignment";
+// import Assessment from "@material-ui/icons/Assessment";
+// import Ballot from "@material-ui/icons/Ballot";
+// import Class from "@material-ui/icons/Class";
+// import Bolt from "@material-ui/icons/Dvr";
 
 const stories = storiesOf("Loah", module);
 
@@ -16,48 +16,48 @@ stories.add("Light", () => {
   const buttons = [
     {
       name: "Users",
-      icon: <AddAlertIcon />,
+      // icon: <AddAlertIcon />,
       slug: "users",
       action: () => setCurrentPage("users")
     },
-    {
-      name: "Menus",
-      icon: <AllInbox />,
-      slug: "menus",
-      action: () => setCurrentPage("menus")
-    },
-    {
-      name: "Books",
-      icon: <Assignment />,
-      slug: "books",
-      action: () => setCurrentPage("books")
-    },
-    {
-      name: "Items",
-      icon: <Assessment />,
-      slug: "items",
-      action: () => setCurrentPage("items")
-    },
-    {
-      name: "Add Alert",
-      icon: <Ballot />,
-      slug: "alert",
-      action: () => setCurrentPage("alert")
-    },
-    {
-      name: "Coins",
-      icon: <Class />,
-      slug: "coins",
-      action: () => setCurrentPage("coins")
-    },
+    // {
+    //   name: "Menus",
+    //   icon: <AllInbox />,
+    //   slug: "menus",
+    //   action: () => setCurrentPage("menus")
+    // },
+    // {
+    //   name: "Books",
+    //   icon: <Assignment />,
+    //   slug: "books",
+    //   action: () => setCurrentPage("books")
+    // },
+    // {
+    //   name: "Items",
+    //   icon: <Assessment />,
+    //   slug: "items",
+    //   action: () => setCurrentPage("items")
+    // },
+    // {
+    //   name: "Add Alert",
+    //   icon: <Ballot />,
+    //   slug: "alert",
+    //   action: () => setCurrentPage("alert")
+    // },
+    // {
+    //   name: "Coins",
+    //   icon: <Class />,
+    //   slug: "coins",
+    //   action: () => setCurrentPage("coins")
+    // },
 
-    {
-      name: "Tokens",
-      slug: "tokens",
-      icon: <Bolt />,
-      action: () => setCurrentPage("tokens")
-    },
-    { name: "Repos" }
+    // {
+    //   name: "Tokens",
+    //   slug: "tokens",
+    //   icon: <Bolt />,
+    //   action: () => setCurrentPage("tokens")
+    // },
+    // { name: "Repos" }
   ];
 
   return (
