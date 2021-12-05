42ebba8cbc4bc27205953b548ac2c8b656145d69
BlackBo6 <achrafboucetta00@gmail.com>
2021-12-05 02:55:08 +0100

    update storybook

--- src/stories/RadioSelector.stories.js
+++ src/stories/RadioSelector.stories.js
@@ -1,53 +1,23 @@
 import React, { useState } from "react";
-import { storiesOf } from "@storybook/react";
-import { Hawa } from "../components/Hawa/Hawa";
-import { StyledCheckbox } from "../components/Hawa/Checkbox/Checkbox";
-import { AutoCompleteField } from "../components/Hawa/AutoCompleteField/AutoCompleteField";
-import { StyledInputLabel } from "../components/Hawa/InputLabel/StyledInputLabel";
-import { StyledTextArea } from "../components/Hawa/TextArea/TextArea";
-import { StyledTextField } from "../components/Hawa/TextField/TextField";
 import { StyledRadioSelector } from "../components/Hawa/RadioSelector";
-import { FormProvider, useForm } from "react-hook-form";
 import "../styles.css";
 
-const stories = storiesOf("RadioSelector", module);
-
-stories.add("Light", (args) => {
-  return <StyledRadioSelector {...args} />;
-});
-
 export default {
-  title: "test",
-  component: [
-    StyledCheckBox,
-    StyledTextFieldT,
-    StyledTextAreaT,
-    StyledRadioSeletorT
-  ],
+  title: "RadioSelector",
+  component: StyledRadioSeletor,
   argTypes: {
-    resize: {
-      options: ["vertical", "horizontal", "both"],
-      control: { type: "radio" }
+    options: {
+      control: "array"
     }
   }
 };
 
-/****************************/
-// Radio Selector Template
-const styledRadioSelectorTempalte = (args) => {
-  const methods = useForm();
-
-  return (
-    <FormProvider {...methods}>
-      <StyledRadioSelector {...args} />
-    </FormProvider>
-  );
+const Template = (args) => {
+  return <StyledRadioSelector {...args} />;
 };
 
-export const StyledRadioSeletorT = styledRadioSelectorTempalte.bind({});
-StyledRadioSeletorT.args = {
-  name: "Radio Selector",
-  shouldUnregister: true,
+export const StyledRadioSeletor = Template.bind({});
+StyledRadioSeletor.args = {
   options: [
     { text: "Option 1", label: "option1" },
     { text: "Option 2", label: "option2" },
@@ -57,6 +27,9 @@ StyledRadioSeletorT.args = {
   bdRadius: 12,
   defaultValue: "option2",
   bgSelectedColor: "red",
-  textSelectedColor: "black"
+  textSelectedColor: "black",
+  handleChange: () => {
+    console.log("handleChange goes here");
+  }
 };
 /****************************/
