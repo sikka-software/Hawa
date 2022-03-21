6c0d34f3d2c6c434e75094f730db2ca0297eab62
Zakher Masri <zakhermasri@gmail.com>
2022-03-21 23:36:20 +0300

    exported hawaprovider

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export * from "./blocks/Account";
 export * from "./blocks/AuthForms";
 export * from "./blocks/Payment";
+export { HawaProvider, defaultTheme } from "./themes/HawaProvider";
 //export * from "./components/Hawa";
