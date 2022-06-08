c9e0112823f31c9f719ea9a9e83962e2ed5ea58b
BlackBo6 <achrafboucetta00@gmail.com>
2022-06-08 16:02:35 +0100

    #2j3x1dk[COMPLETED]

---
+++ src/elements/HawaSelectInput.js
@@ -0,0 +1,30 @@
+import Select from "react-select";
+import Creatable from "react-select/creatable";
+
+export default function HawaSelectInput(props) {
+  return (
+      <>
+    {!props.isCreatable && (
+      <Select
+        options={props.options}
+        isClearable={props.isClearable}
+        isMulti={props.isMulti}
+        isSearchable={props.isSearchable}
+        onChange={(newValue, action) => props.onChange(newValue, action)}
+      />
+    )}
+    {props.isCreatable && (
+        <Creatable
+      options={props.options}
+      isClearable={props.isClearable}
+      isMulti={props.isMulti}
+      isSearchable={props.isSearchable}
+      onChange={(newValue, action) => props.onChange(newValue, action)}
+      onInputChange={(newValue, action) =>
+        props.onInputChange(newValue, action)
+      }
+    />
+    )}
+    </>
+  );
+}
