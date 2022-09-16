326f7675ee8800cafc710542b7c5381c935ef767
BlackBo6 <achrafboucetta00@gmail.com>
2022-09-16 03:06:47 +0100

    UI updates

---
+++ src/layout/HawaBottomAppBar.js
@@ -0,0 +1,76 @@
+import { AppBar, Paper, Box, IconButton, Typography } from "@mui/material";
+
+export function HawaBottomAppBar({ args }) {
+  return (
+    <AppBar
+      position="fixed"
+      sx={args.sx}
+      style={{
+        bottom: 0,
+        top: "auto",
+        padding: 10,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-evenly"
+      }}
+      color={args.color}
+    >
+      <BottomAppBar appBarContent={args.appBarContent} />
+    </AppBar>
+  );
+}
+
+export function BottomAppBar(props) {
+  return (
+    <Paper
+      elevation={3}
+      style={{
+        width: "100%",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        borderRadius: 10,
+        alignContent: "center",
+        padding: 10
+      }}
+      variant="outlined"
+    >
+      {props.appBarContent.map(({ label, icon, action }) => (
+        <SubAppBarContent label={label} icon={icon} action={action} />
+      ))}
+    </Paper>
+  );
+}
+
+function SubAppBarContent(props) {
+  return (
+    <Box
+      display="flex"
+      flexDirection="row"
+      justifyContent="center"
+      alignItems="center"
+      sx={{
+        "&:hover": {
+          cursor: "pointer",
+          color: "blue"
+        }
+      }}
+      onClick={props.action}
+    >
+      <IconButton
+        sx={{
+          ml: 1,
+          "&.MuiButtonBase-root:hover": {
+            color: "blue",
+            bgcolor: "transparent"
+          }
+        }}
+      >
+        {props.icon}
+      </IconButton>
+      <Typography sx={{ display: { xs: "none", sm: "none", md: "block" } }}>
+        {props.label}
+      </Typography>
+    </Box>
+  );
+}
---
+++ src/stories/LayoutStories/SupAppBar.stories.js
@@ -0,0 +1,40 @@
+import { BottomAppBar } from "../../layout/HawaBottomAppBar";
+import AppSettingsAltIcon from "@mui/icons-material/AppSettingsAlt";
+import BugReportIcon from "@mui/icons-material/BugReport";
+import AppsIcon from "@mui/icons-material/Apps";
+import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
+
+export default {
+  title: "Layout/SupAppBar",
+  component: [BottomAppBar]
+};
+
+const Template = (args) => {
+  return <BottomAppBar {...args} />;
+};
+
+export const Normal = Template.bind({});
+Normal.args = {
+  appBarContent: [
+    {
+      label: "Button 1",
+      icon: <AppSettingsAltIcon />,
+      action: () => console.log("button 1")
+    },
+    {
+      label: "Button 2",
+      icon: <BugReportIcon />,
+      action: () => console.log("button 2")
+    },
+    {
+      label: "Button 3",
+      icon: <AppsIcon />,
+      action: () => console.log("button 3")
+    },
+    {
+      label: "Button 4",
+      icon: <EmojiEmotionsIcon />,
+      action: () => console.log("button 4")
+    }
+  ]
+};
