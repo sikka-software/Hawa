5e752b92c0d821f052ad0e40962ef9caa6be2398
BlackBo6 <achrafboucetta00@gmail.com>
2022-09-16 17:43:09 +0100

    add arguments

--- src/stories/LayoutStories/Drawer.stories.js
+++ src/stories/LayoutStories/Drawer.stories.js
@@ -1,5 +1,8 @@
 import { Typography } from "@mui/material";
 import { HawaDrawer } from "../../layout/HawaDrawer";
+import CategoryIcon from "@mui/icons-material/Category";
+import StyleIcon from "@mui/icons-material/Style";
+import SettingsIcon from "@mui/icons-material/Settings";
 
 export default {
   title: "Layout/Drawer",
@@ -8,10 +11,31 @@ export default {
 
 const Template = (args) => {
   return (
-    <HawaDrawer>
+    <HawaDrawer {...args}>
       <Typography>Here goes content</Typography>
     </HawaDrawer>
   );
 };
 
 export const Drawer = Template.bind({});
+Drawer.args = {
+  drawerContent: [
+    {
+      label: "Menu Item",
+      icon: <CategoryIcon />,
+      action: () => console.log("this is menu item")
+    },
+    {
+      label: "Style",
+      icon: <StyleIcon />,
+      action: () => console.log("this is Style")
+    },
+    {
+      label: "Settings",
+      icon: <SettingsIcon />,
+      action: () => console.log("this is Settings")
+    }
+  ],
+  handleDrawerToggle: () => console.log("called when drawer toggled"),
+  drawerMaxWidth: 240
+};
