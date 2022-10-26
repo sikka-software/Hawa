acd220ba091e3904539c3333ed5805f073a44426
Zakher Masri <zakhermasri@WING.local>
2022-10-26 14:49:01 +0300

    SIKKA-3804[closed]

--- src/elements/HawaTextField.js
+++ src/elements/HawaTextField.js
@@ -1,32 +1,25 @@
 import React from "react";
-import Input from "@mui/material/Input";
-import InputLabel from "@mui/material/InputLabel";
-import Typography from "@mui/material/Typography";
 
 export const HawaTextField = (props) => {
   return (
-    <div style={props.inForm && { width: "100%" }}>
-      <div style={{ width: props.fullWidth ? "100%" : "fit-content" }}>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            alignItems: "center"
-          }}
-        >
-          {props.label && <InputLabel>{props.label}</InputLabel>}
-          {props.helperText && (
-            <Typography
-              style={{ marginBottom: !props.label && 10 }}
-              variant="validation"
-            >
-              {props.helperText}
-            </Typography>
-          )}
-        </div>
-        <Input disableUnderline {...props} />
-      </div>
+    <div>
+      <label
+        for="first_name"
+        class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+      >
+        {props.label}
+      </label>
+      <input
+        {...props}
+        class="mb-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      />
+
+      {props.helperText && (
+        <p class="mt-2 text-sm text-red-600 dark:text-red-500">
+          {/* <span class="font-medium">Oh, snapp!</span> */}
+          {props.helperText}
+        </p>
+      )}
     </div>
   );
 };
