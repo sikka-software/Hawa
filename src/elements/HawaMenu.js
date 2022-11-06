befd7987ca8e419a68ae767b00352a5ee001e794
BlackBo6 <achrafboucetta00@gmail.com>
2022-11-06 16:15:44 +0100

    dropdown menu

--- src/elements/HawaMenu.js
+++ src/elements/HawaMenu.js
@@ -6,42 +6,54 @@ export const HawaMenu = ({
   withHeader,
   withIcons,
   headerTitle,
-  headerSubtitle
+  headerSubtitle,
+  open,
+  handleClose
 }) => {
   return (
-    <div
-      id={popMenuID}
-      className="z-10 w-44 bg-white rounded-lg divide-y divide-gray-100 shadow dark:bg-gray-700"
-    >
-      {withHeader && (
-        <div class="py-3 px-4 text-sm text-gray-900 dark:text-white">
-          <div>{headerTitle}</div>
-          <div class="font-medium truncate">{headerSubtitle}</div>
-        </div>
-      )}
-      {menuItems.map((group) => {
-        return (
-          <ul className="py-1  text-sm text-gray-700 dark:text-gray-200">
-            {group.map((item) => {
-              return (
-                <li
-                  onClick={item.action}
-                  className={
-                    item.button
-                      ? "bg-primary-500 text-white hover:bg-primary-600 flex flex-row rtl:flex-row-reverse items-center cursor-pointer py-2 px-4 rounded-lg mx-1 hover:bg-gray-100 dark:hover:bg-primary-600 dark:hover:text-white"
-                      : "flex flex-row rtl:flex-row-reverse items-center cursor-pointer py-2 px-4 rounded-lg mx-1 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                  }
-                >
-                  {withIcons && (
-                    <div className="mr-2 rtl:ml-2">{item.icon}</div>
-                  )}
-                  {item.label}
-                </li>
-              );
-            })}
-          </ul>
-        );
-      })}
+    <div>
+      <button
+        className="border rounded w-44 p-2 bg-blue-700 text-white"
+        onClick={() => handleClose(!open)}
+      >
+        Menu Dropdown
+      </button>
+      <div
+        id={popMenuID}
+        className={`${
+          open ? "block" : "hidden"
+        } z-10 w-44 bg-white rounded-lg divide-y divide-gray-100 shadow dark:bg-gray-700`}
+      >
+        {withHeader && (
+          <div class="py-3 px-4 text-sm text-gray-900 dark:text-white">
+            <div>{headerTitle}</div>
+            <div class="font-medium truncate">{headerSubtitle}</div>
+          </div>
+        )}
+        {menuItems.map((group) => {
+          return (
+            <ul className="py-1  text-sm text-gray-700 dark:text-gray-200">
+              {group.map((item) => {
+                return (
+                  <li
+                    onClick={item.action}
+                    className={
+                      item.button
+                        ? "bg-primary-500 text-white hover:bg-primary-600 flex flex-row rtl:flex-row-reverse items-center cursor-pointer py-2 px-4 rounded-lg mx-1 hover:bg-gray-100 dark:hover:bg-primary-600 dark:hover:text-white"
+                        : "flex flex-row rtl:flex-row-reverse items-center cursor-pointer py-2 px-4 rounded-lg mx-1 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                    }
+                  >
+                    {withIcons && (
+                      <div className="mr-2 rtl:ml-2">{item.icon}</div>
+                    )}
+                    {item.label}
+                  </li>
+                );
+              })}
+            </ul>
+          );
+        })}
+      </div>
     </div>
   );
 };
