f34da8e2933762aa357a15412f561be9df171cf1
Zakher Masri <zakhermasri@WING.local>
2022-11-26 23:04:44 +0300

    SIKKA-3915[closed]

--- src/elements/HawaSwitch.tsx
+++ src/elements/HawaSwitch.tsx
@@ -17,9 +17,11 @@ export const HawaSwitch: React.FunctionComponent<SwitchTypes> = (props) => {
         className="peer sr-only"
       />
       <div className="peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:top-[2px] after:left-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-blue-800"></div>
-      <span className="mx-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-        {props.text}
-      </span>
+      {props.text && (
+        <span className="mx-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+          {props.text}
+        </span>
+      )}
     </label>
   )
 }
