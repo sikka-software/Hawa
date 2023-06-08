5f7b1101674a79a4323917a240f238bcc6bd38e1
Zakher <zakhermasri@gmail.com>
2023-06-09 00:15:12 +0300

    SIKKA-4253[closed]

--- src/blocks/Misc/NoPermission.tsx
+++ src/blocks/Misc/NoPermission.tsx
@@ -1,27 +1,30 @@
 import React from "react"
 import { HawaButton } from "../../elements"
 import { HawaContainer } from "../../layout"
-import { FaCheck } from "react-icons/fa"
+import {  FaLock } from "react-icons/fa"
 
-type TEmptyState = {
+type TNoPermission = {
   variant?: "outlined" | "contained" | "neobrutalism"
 }
 
-export const EmptyState: React.FunctionComponent<TEmptyState> = ({
+export const NoPermission: React.FunctionComponent<TNoPermission> = ({
   variant = "contained",
 }) => {
   return (
     <HawaContainer variant={variant} centered={true}>
       <div className="flex flex-col items-center justify-center text-center dark:text-white">
         <div className="flex h-10 w-10 flex-col items-center justify-center rounded-3xl bg-buttonPrimary-300 text-6xl font-bold">
-          <FaCheck size={20} color="white" />
+          <FaLock size={20} color="white" />
         </div>
-        <div className="m-2 text-xl font-bold">You're all caught up</div>
-        <div>If you're lost, please contact us help@sikka.io </div>
+        <div className="m-2 text-xl font-bold">You don't have permission</div>
+        <div>
+          If you think this is a problem please contact your administrator or
+          our customer support
+        </div>
       </div>
-      <HawaButton color="primary" width="full">
+      {/* <HawaButton color="primary" width="full">
         Action
-      </HawaButton>
+      </HawaButton> */}
     </HawaContainer>
   )
 }
---
+++ src/stories/BlocksStories/Misc/NoPermission.stories.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { NoPermission as NoPerm } from "../../../blocks/Misc";
+
+export default {
+  title: "Blocks/Misc/No Permission",
+  component: [NoPerm],
+  parameters: {
+    docs: {
+      description: {
+        component: "`<SignInForm/>` Authentication block for user sign in"
+      }
+    }
+  },
+
+  argTypes: {
+    variant: {
+      default: "contained",
+      control: "select",
+      options: ["contained", "outlined", "neobrutalism"],
+      description: "The theme style of the block"
+    },
+    lang: {
+      default: true,
+      control: "select",
+      options: ["ar", "en"],
+      description: "The language of the form",
+      table: { defaultValue: { summary: "en" } }
+    },
+    handleRouteToHome: {
+      action: "Redirecting to home page",
+      description: "Directing user to home page"
+    }
+  }
+};
+
+export const NoPermission = (args) => {
+  return <NoPerm {...args} />;
+};
+
+NoPermission.args = {
+  variant: "contained"
+};
