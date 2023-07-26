7048f5425c89891bc17556dfe1e34386bb8e7601
Zakher Masri <zakhermasri@gmail.com>
2023-07-26 05:57:44 +0300

    SIKKA-4618[completed] Updated rollup plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,11 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import babel from "rollup-plugin-babel";
+// import babel from "rollup-plugin-babel";
+import { babel } from "@rollup/plugin-babel";
+
 import external from "rollup-plugin-peer-deps-external";
-import { terser } from "rollup-plugin-terser";
+// import { terser } from "rollup-plugin-terser";
+import terser from "@rollup/plugin-terser";
 import postcss from "rollup-plugin-postcss";
 import typescript from "rollup-plugin-typescript2";
 import swc from "rollup-plugin-swc";
@@ -21,6 +24,7 @@ export default [
       }),
       postcss({ plugins: [], minimize: true }),
       babel({
+        babelHelpers: "bundled",
         exclude: "node_modules/**",
         presets: [
           "@babel/preset-env",
