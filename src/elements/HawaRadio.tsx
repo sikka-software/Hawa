3464e8244d372a7a8081a7d1cf4da8ea5cb47255
Zakher Masri <zakhermasri@gmail.com>
2023-08-10 20:22:15 +0300

    updated HawaRadio

--- src/elements/HawaRadio.tsx
+++ src/elements/HawaRadio.tsx
@@ -2,49 +2,174 @@ import React, { useState, FC } from "react"
 import clsx from "clsx"
 
 type RadioTypes = {
+  orientation?: "vertical" | "horizontal"
+  design?: "default" | "tabs" | "cards" | "bordered"
   options?: [
     {
       value: any
       label: any
+      disabled?: any
+      sublabel?: any
+      beforeIcon?: any
+      afterIcon?: any
     }
   ]
   onChangeTab?: any
   defaultValue?: any
 }
-export const HawaRadio: FC<RadioTypes> = (props) => {
+export const HawaRadio: FC<RadioTypes> = ({
+  design = "default",
+  orientation = "horizontal",
+  ...props
+}) => {
   const [selectedOption, setSelectedOption] = useState(props.defaultValue)
   let activeTabStyle =
     "inline-block py-2 px-4  w-full text-white bg-buttonPrimary-500 rounded active"
   let inactiveTabStyle =
     "inline-block py-2 px-4 w-full bg-gray-100 rounded hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white"
+  let orientationStyle = {
+    horizontal: "flex flex-row",
+    vertical: "flex flex-col",
+  }
+  switch (design) {
+    case "tabs":
+      return (
+        <div>
+          <ul
+            className={clsx(
+              props.options?.length > 2
+                ? "flex-wrap xs:max-w-full xs:flex-nowrap"
+                : "",
+              " max-w-fit  whitespace-nowrap rounded bg-gray-100 text-center text-sm font-medium text-gray-500 dark:text-gray-400",
+              orientationStyle[orientation]
+            )}
+          >
+            {props.options?.map((opt: any, o) => (
+              <li className="w-full" key={o}>
+                <button
+                  aria-current="page"
+                  onClick={() => {
+                    setSelectedOption(opt.value)
+                    props.onChangeTab(opt.value)
+                  }}
+                  className={
+                    selectedOption === opt.value
+                      ? activeTabStyle
+                      : inactiveTabStyle
+                  }
+                >
+                  {opt.label}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )
+    case "bordered":
+      return (
+        <div className={clsx(orientationStyle[orientation], "gap-4")}>
+          {props.options.map((opt, i) => (
+            <div className="rounded border border-gray-200  ">
+              <div
+                className="radio-item radio-item-bordered flex items-center   transition-all"
+                key={i + 1}
+              >
+                <input
+                  disabled={opt.disabled}
+                  id={opt.value.toString()}
+                  type="radio"
+                  value={opt.value}
+                  name="bordered-radio"
+                  // className="h-4 w-4 border-gray-300 "
+                />
+                <label
+                  htmlFor={opt.value.toString()}
+                  className={clsx(
+                    "ml-2 w-full  p-4 pl-3  text-sm font-medium",
+                    opt.disabled ? "opacity-50" : "cursor-pointer text-gray-900"
+                  )}
+                >
+                  {opt.label}
+                </label>
+              </div>
+            </div>
+          ))}
+        </div>
+      )
+    case "cards":
+      return (
+        <ul className={clsx(orientationStyle[orientation], "gap-4")}>
+          {props.options?.map((opt: any, o) => (
+            <li>
+              <input
+                type="radio"
+                id={opt.value.toString()}
+                name="cards-radio"
+                value={opt.value.toString()}
+                className="peer hidden"
+                required
+              />
+              <label
+                htmlFor={opt.value.toString()}
+                className="inline-flex h-full w-full cursor-pointer items-center justify-between rounded-lg border border-gray-200 bg-white p-5 text-gray-500 hover:bg-gray-100 hover:text-gray-600 peer-checked:border-blue-600 peer-checked:text-blue-600 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300 dark:peer-checked:text-blue-500"
+              >
+                <div className="block  h-full w-full">
+                  <div className="w-full text-lg font-semibold">
+                    {opt.label}
+                  </div>
+                  <div className="w-full">{opt.sublabel}</div>
+                </div>
+                {/* <svg
+                  className="ml-3 h-5 w-5 "
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M1 5h12m0 0L9 1m4 4L9 9"
+                  />
+                </svg> */}
+              </label>
+            </li>
+          ))}
+        </ul>
+      )
 
-  return (
-    <div>
-      <ul
-        className={clsx(
-          props.options?.length > 2
-            ? "flex-wrap xs:max-w-full xs:flex-nowrap"
-            : "",
-          "flex max-w-fit flex-row whitespace-nowrap rounded bg-gray-100 text-center text-sm font-medium text-gray-500 dark:text-gray-400"
-        )}
-      >
-        {props.options?.map((opt: any, o) => (
-          <li className="w-full" key={o}>
-            <button
-              aria-current="page"
-              onClick={() => {
-                setSelectedOption(opt.value)
-                props.onChangeTab(opt.value)
-              }}
-              className={
-                selectedOption === opt.value ? activeTabStyle : inactiveTabStyle
-              }
+    default:
+      return (
+        <div className={orientationStyle[orientation]}>
+          {props.options.map((opt, i) => (
+            <div
+              className="radio-item radio-item-default mb-4 flex items-center transition-all"
+              key={i + 1}
             >
-              {opt.label}
-            </button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
+              <input
+                disabled={opt.disabled}
+                id={opt.value.toString()}
+                type="radio"
+                value={opt.value}
+                name="default-radio"
+                // className="h-4 w-4 border-gray-300 "
+              />
+              <label
+                htmlFor={opt.value.toString()}
+                className={clsx(
+                  "ml-2 text-sm font-medium ",
+                  opt.disabled
+                    ? "text-gray-400"
+                    : "cursor-pointer text-gray-900"
+                )}
+              >
+                {opt.label}
+              </label>
+            </div>
+          ))}
+        </div>
+      )
+  }
 }
---
+++ src/stories/ElementsStories/Radio.stories.js
@@ -0,0 +1,183 @@
+import React from "react";
+
+import { HawaRadio } from "../../elements";
+
+export default {
+  title: "Elements/Radio",
+  component: HawaRadio
+};
+
+export const Default = (args) => {
+  return (
+    <div className="flex w-1/2 flex-col gap-10">
+      <div className="flex flex-col gap-2">
+        <h1>Orientation: Horizontal</h1>
+        <HawaRadio
+          onChangeTab={() => console.log()}
+          defaultValue={"option2"}
+          design={"default"}
+          // {...args}
+          options={[
+            { label: `Option 1`, value: `option1` },
+            { label: `Option 2`, value: `option2`, disabled: true },
+            { label: `Option 3`, value: `option3` }
+          ]}
+        />
+      </div>
+      <div className="flex flex-col gap-2">
+        <h1>Orientation: Vertical</h1>
+        <HawaRadio
+          onChangeTab={() => console.log()}
+          defaultValue={"option6"}
+          // options={args.options}
+          design={"default"}
+          orientation="vertical"
+          // {...args}
+          options={[
+            { label: `Option 4`, value: `option4` },
+            { label: `Option 5`, value: `option5`, disabled: true },
+            { label: `Option 6`, value: `option6` }
+          ]}
+        />
+      </div>
+    </div>
+  );
+};
+
+export const Bordered = (args) => {
+  return (
+    <div className="flex w-1/2 flex-col gap-10">
+      <div className="flex flex-col gap-2">
+        <h1>Orientation: Horizontal</h1>
+        <HawaRadio
+          onChangeTab={() => console.log()}
+          defaultValue={"option2"}
+          design={"bordered"}
+          // {...args}
+          options={[
+            { label: `Option 1`, value: `option1` },
+            { label: `Option 2`, value: `option2`, disabled: true },
+            { label: `Option 3`, value: `option3` }
+          ]}
+        />
+      </div>
+      <div className="flex flex-col gap-2">
+        <h1>Orientation: Vertical</h1>
+        <HawaRadio
+          onChangeTab={() => console.log()}
+          defaultValue={"option6"}
+          // options={args.options}
+          design={"bordered"}
+          orientation="vertical"
+          // {...args}
+          options={[
+            { label: `Option 4`, value: `option4` },
+            { label: `Option 5`, value: `option5`, disabled: true },
+            { label: `Option 6`, value: `option6` }
+          ]}
+        />
+      </div>
+    </div>
+  );
+};
+
+export const Tabs = (args) => {
+  return (
+    <div className="flex w-1/2 flex-col gap-10">
+      <div className="flex flex-col gap-2">
+        <h1>Orientation: Horizontal</h1>
+        <HawaRadio
+          onChangeTab={() => console.log()}
+          defaultValue={"option2"}
+          design={"tabs"}
+          // {...args}
+          options={[
+            { label: `Option 1`, value: `option1` },
+            { label: `Option 2`, value: `option2`, disabled: true },
+            { label: `Option 3`, value: `option3` }
+          ]}
+        />
+      </div>
+      <div className="flex flex-col gap-2">
+        <h1>Orientation: Vertical</h1>
+        <HawaRadio
+          onChangeTab={() => console.log()}
+          defaultValue={"option6"}
+          // options={args.options}
+          design={"tabs"}
+          orientation="vertical"
+          // {...args}
+          options={[
+            { label: `Option 4`, value: `option4` },
+            { label: `Option 5`, value: `option5`, disabled: true },
+            { label: `Option 6`, value: `option6` }
+          ]}
+        />
+      </div>
+    </div>
+  );
+};
+export const Cards = (args) => {
+  return (
+    <div className="flex w-1/2 flex-col gap-10">
+      <div className="flex flex-col gap-2">
+        <h1>Orientation: Horizontal</h1>
+        <HawaRadio
+          onChangeTab={() => console.log()}
+          defaultValue={"option2"}
+          design={"cards"}
+          // {...args}
+          options={[
+            {
+              label: `Option 1`,
+              sublabel: "Good for small business",
+              value: `option1`
+            },
+            {
+              label: `Option 2`,
+
+              sublabel: "Good for medium to large businesses",
+              value: `option2`,
+              disabled: true
+            },
+            {
+              label: `Option 3`,
+              sublabel: "Custom made for Enterprise level companies",
+              value: `option3`
+            }
+          ]}
+        />
+      </div>
+      <div className="flex flex-col gap-2">
+        <h1>Orientation: Vertical</h1>
+        <HawaRadio
+          onChangeTab={() => console.log()}
+          defaultValue={"option6"}
+          // options={args.options}
+          design={"cards"}
+          orientation="vertical"
+          // {...args}
+          options={[
+            {
+              label: `Option 4`,
+              sublabel: "Good for small business",
+              value: `option4`
+            },
+            {
+              label: `Option 5`,
+
+              sublabel: "Good for medium to large businesses",
+              value: `option5`,
+              disabled: true
+            },
+            {
+              label: `Option 6`,
+              sublabel: "Custom made for Enterprise level companies",
+              value: `option6`
+            }
+          ]}
+        />
+      </div>
+    </div>
+  );
+};
