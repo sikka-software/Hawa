9bc9b9fe8e1466a6564f1a9cbee1296f4a790a58
zaaakher <zakhermasri@gmail.com>
2023-09-09 22:56:26 +0300

    updated landing + made button component not use client

--- landing/pages/index.js
+++ landing/pages/index.js
@@ -1,16 +1,27 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { Button, DropdownMenu } from "@sikka/hawa";
+import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [isClient, setIsClient] = useState(false)
+ 
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+ 
+  // return 
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
+
+<h1>{isClient ? 'This is never prerendered' : 'Prerendered'}</h1>
       <DropdownMenu
-        trigger={<Button>This is hawa</Button>}
+        trigger={<div>This is hawa</div>}
         items={[
           { label: "Something", value: "something" },
           { label: "Something", value: "something" },
@@ -18,7 +29,65 @@ export default function Home() {
           { label: "Something", value: "something" }
         ]}
       />{" "}
-      <div className="font-mono z-10 w-full max-w-5xl items-center justify-between text-sm lg:flex">
+      <header
+        role="banner"
+        //   style="
+        //   display: flex;
+        //   flex-direction: column;
+        //   height: 100%;
+        //   justify-content: center;
+        //   align-items: center;
+        // "
+      >
+        <div align="center">
+          <img
+            src="https://xakher-images.s3.ap-southeast-1.amazonaws.com/hawa-logo.png"
+            alt="Hawa | هواء"
+          />
+        </div>
+        <div className="page-header h-fit" role="banner">
+          <h1 className="project-name">Hawa | هواء</h1>
+          <h2 className="project-tagline">
+            React UI Kit made for SaaS projects
+          </h2>
+          <div className="buttons my-6">
+            <a
+              href="https://github.com/sikka-software/hawa"
+              className="btn-outline"
+            >
+              GitHub
+            </a>
+
+            <a href="https://hawa.style/documentation" className="btn">
+              Components
+            </a>
+          </div>
+          <a href="https://www.npmjs.com/package/@sikka/hawa">
+            <img
+              width="100"
+              alt="Sikka"
+              src="https://img.shields.io/npm/v/@sikka/hawa.svg?style=flat&colorA=000000&colorB=000000"
+            />
+          </a>
+        </div>
+      </header>
+      <div>
+        <div align="center" className="m-0">
+          <a href="https://sikka.io">
+            <img
+              width="30"
+              alt="Sikka"
+              src="https://i.postimg.cc/8cK4tnKQ/sikka-symbol-black.png"
+            />
+          </a>
+        </div>
+        <p align="center" className="m-0 mb-4">
+          <sub>
+            An open source project by <a href="https://sikka.io">Sikka</a>
+          </sub>
+        </p>
+      </div>
+      {/* <div className="font-mono z-10 w-full max-w-5xl items-center justify-between text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
           <code className="font-mono font-bold">pages/index.js</code>
@@ -120,7 +189,7 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
-      </div>
+      </div> */}
     </main>
   );
 }
--- src/elements/Button.tsx
+++ src/elements/Button.tsx
@@ -1,4 +1,4 @@
-"use client"
+// "use client"
 
 import * as React from "react"
 // import { Slot } from "@radix-ui/react-slot"
