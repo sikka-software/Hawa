1f17b38a2af46b0c26e89dcbb16a928ee835c548
zaaakher <zakhermasri@gmail.com>
2023-09-16 01:42:07 +0300

    SIKKA-4999[completed] Abstracted the headerTools of HawaTable

--- src/stories/ElementsStories/Table.stories.js
+++ src/stories/ElementsStories/Table.stories.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HawaTable } from "../../elements";
+import { Button, HawaTable, HawaTextField } from "../../elements";
 import { setLocale, t } from "../../translations/i18n";
 
 export default {
@@ -258,7 +258,58 @@ export const WithHeader = (args) => {
   return (
     <div dir="ltr" className="">
       <HawaTable
-        headerTools={true}
+        headerTools={
+          <>
+            <HawaTextField
+              icon={
+                <svg
+                  aria-label="Search Icon"
+                  stroke="currentColor"
+                  fill="gray"
+                  strokeWidth="0"
+                  viewBox="0 0 512 512"
+                  height="1em"
+                  width="1em"
+                >
+                  <path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
+                </svg>
+              }
+              placeholder={"Search"}
+              width="full"
+              margin="none"
+            />{" "}
+            <div className="flex flex-row items-center justify-between gap-2">
+              <Button className="flex flex-row gap-2">
+                <svg
+                  aria-label="Filter Icon"
+                  stroke="currentColor"
+                  fill="currentColor"
+                  strokeWidth="0"
+                  viewBox="0 0 16 16"
+                  height="1em"
+                  width="1em"
+                >
+                  <path d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"></path>
+                </svg>
+                {"Filter"}
+                <svg
+                  aria-label="Chevron Right Icon"
+                  stroke="currentColor"
+                  fill="currentColor"
+                  strokeWidth="0"
+                  viewBox="0 0 16 16"
+                  height="1em"
+                  width="1em"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+                  ></path>
+                </svg>
+              </Button>
+            </div>
+          </>
+        }
         direction="ltr"
         actions={[
           { label: "View", action: (e) => console.log("viewing", e) },
