da96ced8f4f805cc865193e6b82272d9d76a2503
zaaakher <zakhermasri@gmail.com>
2023-09-19 16:49:49 +0300

    updating bundling skip_landing

--- tsup.config.js
+++ tsup.config.js
@@ -1,6 +1,8 @@
 module.exports = {
+  entryPoints: ["src/index.ts"], // Specify the entry file(s) of your library
   splitting: true,
   sourcemap: true,
+  minify: true,
   clean: true,
   dts: true,
   external: ["react", "react-dom", "react-icons"]
