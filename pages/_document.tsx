2b32acb1dddf31f4804b008fed28901a6286d1cc
zaaakher <zakhermasri@gmail.com>
2023-10-11 03:07:00 +0300

    #SIKKA-5273[completed]

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body className="hawa-m-0">
         <Main />
         <NextScript />
