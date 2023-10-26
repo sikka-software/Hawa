4f06e4397806b12eb2fe69254761ce997fc4cb28
zaaakher <zakhermasri@gmail.com>
2023-10-26 20:36:00 +0300

    Updated some types

--- components/elements/Separator.tsx
+++ components/elements/Separator.tsx
@@ -1,32 +1,27 @@
 import * as React from "react";
-import * as SeparatorPrimitive from "@radix-ui/react-separator";
 import { cn } from "../util";
 
-cn;
+// Define your own prop types
+type SeparatorProps = {
+  className?: string;
+  orientation?: "horizontal" | "vertical";
+};
 
-const Separator = React.forwardRef<
-  React.ElementRef<typeof SeparatorPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root>
->(
-  (
-    { className, orientation = "horizontal", decorative = true, ...props },
-    ref
-  ) => (
-    <SeparatorPrimitive.Root
-      ref={ref}
-      decorative={decorative}
-      orientation={orientation}
-      className={cn(
-        "hawa-shrink-0 hawa-bg-border",
-        orientation === "horizontal"
-          ? "hawa-h-[1px] hawa-w-full"
-          : "hawa-h-full hawa-w-[1px]",
-        className
-      )}
-      {...props}
-    />
-  )
+const Separator: React.FC<SeparatorProps> = ({
+  className,
+  orientation = "horizontal",
+  ...props
+}) => (
+  <div
+    className={cn(
+      "hawa-shrink-0 hawa-bg-border",
+      orientation === "horizontal"
+        ? "hawa-h-[1px] hawa-w-full"
+        : "hawa-h-full hawa-w-[1px]",
+      className
+    )}
+    {...props}
+  />
 );
-Separator.displayName = SeparatorPrimitive.Root.displayName;
 
 export { Separator };
