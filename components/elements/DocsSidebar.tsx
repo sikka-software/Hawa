15e1408ac31dccee687967bba0d0d30756ab60fe
zaaakher <zakhermasri@gmail.com>
2023-10-26 23:22:16 +0300

    #SIKKA-4649[in progress]

---
+++ components/elements/DocsSidebar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { cn } from "../util";
+
+type DocsSidebarType = {
+  test?: any;
+};
+
+export const DocsSidebar: React.FC<DocsSidebarType> = ({ test, ...props }) => {
+  return <div>DocsSidebar</div>;
+};
