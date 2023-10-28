3c93fbf8d191fd0251a14b0331e46ba5672870a0
zaaakher <zakhermasri@gmail.com>
2023-10-28 13:19:52 +0300

    Added SplitButton component

--- components/elements/SplitButton.tsx
+++ components/elements/SplitButton.tsx
@@ -1,37 +1,68 @@
 import * as React from "react";
-import { cva, type VariantProps } from "class-variance-authority";
-
 import { cn } from "../util";
-import { Loading } from "./Loading";
-import { Button } from "./Button";
+import { Button, ButtonProps } from "./Button";
+import { DropdownMenu, MenuItemType } from "./DropdownMenu";
 
-const SplitButton = (props: any) => (
-  <div className=" hawa-h-fit hawa-flex hawa-flex-row hawa-justify-center ">
-    <Button variant={props.variant} className="hawa-rounded-r-none">
-      something
-    </Button>
+interface SplitButtonProps extends ButtonProps {
+  direction?: "ltr" | "rtl";
+  menuItems?: MenuItemType[];
+}
+
+const SplitButton: React.FC<SplitButtonProps> = ({
+  variant,
+  direction = "ltr",
+  menuItems = [],
+  children,
+  ...props
+}) => (
+  <div
+    className={cn(
+      "hawa-h-fit hawa-flex  hawa-justify-center",
+      direction === "rtl" ? "hawa-flex-row-reverse" : "hawa-flex-row"
+    )}
+  >
     <Button
-      asChild
-      variant={props.variant}
-      size={"icon"}
+      variant={variant}
+      onClick={() => console.log("btn clicked")}
       className={
-        "hawa-rounded-l-none hawa-h-10 hawa-w-fit hawa-px-1 hawa-border-l-0"
+        direction === "rtl" ? "hawa-rounded-l-none" : "hawa-rounded-r-none"
       }
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="18"
-        height="18"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
-        <path d="m6 9 6 6 6-6" />
-      </svg>
+      {children}
     </Button>
+    <DropdownMenu
+      size="sm"
+      width="sm"
+      direction={direction}
+      items={menuItems}
+      trigger={
+        <Button
+          asChild
+          variant={variant}
+          size={"icon"}
+          className={cn(
+            "hawa-h-10 hawa-w-fit hawa-px-1",
+            direction === "rtl"
+              ? "hawa-rounded-r-none hawa-border-r-0"
+              : "hawa-border-l-0 hawa-rounded-l-none"
+          )}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="18"
+            height="18"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="m6 9 6 6 6-6" />
+          </svg>
+        </Button>
+      }
+    />
   </div>
 );
 SplitButton.displayName = "SplitButton";
--- stories/ElementsStories/SplitButton.stories.tsx
+++ stories/ElementsStories/SplitButton.stories.tsx
@@ -1,9 +1,10 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { SplitButton } from "../../components/elements";
 import { ArgsTable, Title } from "@storybook/blocks";
+import { setLocale } from "../translations/i18n";
 
 const meta = {
-  title: "Elements/SplitButton",
+  title: "Elements/Split Button",
   component: SplitButton,
   parameters: {
     layout: "centered",
@@ -22,14 +23,35 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof SplitButton>;
 
-const TheStory = () => (
-  <div className="hawa-flex hawa-flex-col hawa-gap-2">
-    <div className="hawa-flex hawa-flex-row hawa-gap-2 hawa-flex-wrap">
-      <SplitButton variant={"outline"}>Default</SplitButton>
-    </div>
-  </div>
-);
+const Template = (args: any, globals: any) => {
+  const locale = globals.globals?.locale === "ar" ? "ar" : "en";
+  setLocale(locale);
+  const direction = locale === "ar" ? "rtl" : "ltr";
 
-export const Variations: Story = {
-  render: () => <TheStory />,
+  return (
+    <div
+      className="hawa-flex hawa-flex-col hawa-w-full hawa-max-w-md"
+      // dir={direction}
+    >
+      <SplitButton
+        direction={direction}
+        variant={"outline"}
+        menuItems={[
+          {
+            label: `Discard changes`,
+            value: 10,
+            action: () => console.log("discarding changes"),
+          },
+          { label: `Save as draft`, value: 10 },
+          { label: `Send for review`, value: 10 },
+        ]}
+        {...args}
+      >
+        Submit
+      </SplitButton>
+    </div>
+  );
+};
+export const Default: Story = {
+  render: Template.bind({}),
 };
