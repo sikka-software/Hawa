93b97d1bb762296092b89c9a3512a74d46e8905d
zaaakher <zakhermasri@gmail.com>
2023-11-06 22:08:19 +0300

    changed label hint icon to question mark

--- components/elements/Label.tsx
+++ components/elements/Label.tsx
@@ -36,8 +36,8 @@ const Label = React.forwardRef<HTMLLabelElement, LabelProps>(
             strokeLinejoin="round"
           >
             <circle cx="12" cy="12" r="10" />
-            <line x1="12" x2="12" y1="8" y2="12" />
-            <line x1="12" x2="12.01" y1="16" y2="16" />
+            <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" />
+            <path d="M12 17h.01" />
           </svg>
         </Tooltip>
       )}
