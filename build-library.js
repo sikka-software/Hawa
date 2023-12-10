ee0b12498bb8173a13fde954189434ec6d9f67f3
zaaakher <zakhermasri@gmail.com>
2023-12-10 15:53:31 +0300

    Attempting to fix build issue

---
+++ build-library.js
@@ -0,0 +1,39 @@
+const { build } = require("tsup");
+
+// const {
+//   buildAllConfig,
+//   buildBlocksConfig,
+//   buildCoreConfig,
+//   buildElementsConfig
+// } = require("./tsup.config");
+
+// async function sequentialBuild() {
+//   await build(buildAllConfig);
+//   await build(buildCoreConfig);
+//   await build(buildBlocksConfig);
+//   await build(buildElementsConfig);
+// }
+
+// sequentialBuild().catch((err) => {
+//   console.error(err);
+//   process.exit(1);
+// });
+
+// APPROACH 03
+// **********************************
+const { groupedConfigs } = require("./tsup.config");
+
+async function buildGroup(config) {
+  await build(config);
+}
+
+async function sequentialBuild() {
+  for (const config of groupedConfigs) {
+    await buildGroup(config);
+  }
+}
+
+sequentialBuild().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
--- tsup.config.js
+++ tsup.config.js
@@ -1,5 +1,7 @@
-import { defineConfig } from "tsup";
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.groupedConfigs = void 0;
+var tsup_1 = require("tsup");
 // BUILD APPROACH 01
 // ********************************************************************************************************************************
 // export default defineConfig([
@@ -37,7 +39,6 @@ import { defineConfig } from "tsup";
 //       "blocks/feedback/index": "components/blocks/feedback/index.ts"
 //     }
 //   },
-
 //   {
 //     name: "Build Elements",
 //     clean: true,
@@ -88,10 +89,8 @@ import { defineConfig } from "tsup";
 //     }
 //   }
 // ]);
-
 // BUILD APPROACH 02
 // ************************************************************************************************************************************************
-
 // export const buildAllConfig = defineConfig({
 //   name: "Build All",
 //   clean: true,
@@ -179,83 +178,72 @@ import { defineConfig } from "tsup";
 //       "components/elements/stopPropagationWrapper/index.ts"
 //   }
 // });
-
 // BUILD APPROACH 03
 // ****************************************************************************************************************
-const elementEntries = {
-  // ELEMENTS
-  "card/index": "components/elements/card/index.ts",
-  "chip/index": "components/elements/chip/index.ts",
-  "tabs/index": "components/elements/tabs/index.ts",
-  "sheet/index": "components/elements/sheet/index.ts",
-  "logos/index": "components/elements/logos/index.ts",
-  "radio/index": "components/elements/radio/index.ts",
-  "table/index": "components/elements/table/index.ts",
-  "alert/index": "components/elements/alert/index.ts",
-  "label/index": "components/elements/label/index.ts",
-  "input/index": "components/elements/input/index.ts",
-  "badge/index": "components/elements/badge/index.ts",
-  "dialog/index": "components/elements/dialog/index.ts",
-  "button/index": "components/elements/button/index.ts",
-  "select/index": "components/elements/select/index.ts",
-  "avatar/index": "components/elements/avatar/index.ts",
-  "switch/index": "components/elements/switch/index.ts",
-  "command/index": "components/elements/command/index.ts",
-  "popover/index": "components/elements/popover/index.ts",
-  "loading/index": "components/elements/loading/index.ts",
-  "tooltip/index": "components/elements/tooltip/index.ts",
-  "skeleton/index": "components/elements/skeleton/index.ts",
-  "combobox/index": "components/elements/combobox/index.ts",
-  "textarea/index": "components/elements/textarea/index.ts",
-  "pinInput/index": "components/elements/pinInput/index.ts",
-  "checkbox/index": "components/elements/checkbox/index.ts",
-  "progress/index": "components/elements/progress/index.ts",
-  "accordion/index": "components/elements/accordion/index.ts",
-  "backToTop/index": "components/elements/backToTop/index.ts",
-  "dataTable/index": "components/elements/dataTable/index.ts",
-  "appStores/index": "components/elements/appStores/index.ts",
-  "sortButton/index": "components/elements/sortButton/index.ts",
-  "scrollArea/index": "components/elements/scrollArea/index.ts",
-  "breadcrumb/index": "components/elements/breadcrumb/index.ts",
-  "phoneInput/index": "components/elements/phoneInput/index.ts",
-  "splitButton/index": "components/elements/splitButton/index.ts",
-  "dropdownMenu/index": "components/elements/dropdownMenu/index.ts",
-  "fileDropzone/index": "components/elements/fileDropzone/index.ts",
-  "navigationMenu/index": "components/elements/navigationMenu/index.ts",
-  "stopPropagationWrapper/index":
-    "components/elements/stopPropagationWrapper/index.ts"
+var elementEntries = {
+    // ELEMENTS
+    "card/index": "components/elements/card/index.ts",
+    "chip/index": "components/elements/chip/index.ts",
+    "tabs/index": "components/elements/tabs/index.ts",
+    "sheet/index": "components/elements/sheet/index.ts",
+    "logos/index": "components/elements/logos/index.ts",
+    "radio/index": "components/elements/radio/index.ts",
+    "table/index": "components/elements/table/index.ts",
+    "alert/index": "components/elements/alert/index.ts",
+    "label/index": "components/elements/label/index.ts",
+    "input/index": "components/elements/input/index.ts",
+    "badge/index": "components/elements/badge/index.ts",
+    "dialog/index": "components/elements/dialog/index.ts",
+    "button/index": "components/elements/button/index.ts",
+    "select/index": "components/elements/select/index.ts",
+    "avatar/index": "components/elements/avatar/index.ts",
+    "switch/index": "components/elements/switch/index.ts",
+    "command/index": "components/elements/command/index.ts",
+    "popover/index": "components/elements/popover/index.ts",
+    "loading/index": "components/elements/loading/index.ts",
+    "tooltip/index": "components/elements/tooltip/index.ts",
+    "skeleton/index": "components/elements/skeleton/index.ts",
+    "combobox/index": "components/elements/combobox/index.ts",
+    "textarea/index": "components/elements/textarea/index.ts",
+    "pinInput/index": "components/elements/pinInput/index.ts",
+    "checkbox/index": "components/elements/checkbox/index.ts",
+    "progress/index": "components/elements/progress/index.ts",
+    "accordion/index": "components/elements/accordion/index.ts",
+    "backToTop/index": "components/elements/backToTop/index.ts",
+    "dataTable/index": "components/elements/dataTable/index.ts",
+    "appStores/index": "components/elements/appStores/index.ts",
+    "sortButton/index": "components/elements/sortButton/index.ts",
+    "scrollArea/index": "components/elements/scrollArea/index.ts",
+    "breadcrumb/index": "components/elements/breadcrumb/index.ts",
+    "phoneInput/index": "components/elements/phoneInput/index.ts",
+    "splitButton/index": "components/elements/splitButton/index.ts",
+    "dropdownMenu/index": "components/elements/dropdownMenu/index.ts",
+    "fileDropzone/index": "components/elements/fileDropzone/index.ts",
+    "navigationMenu/index": "components/elements/navigationMenu/index.ts",
+    "stopPropagationWrapper/index": "components/elements/stopPropagationWrapper/index.ts"
 };
-
-function chunkEntries(entries: any, chunkSize: any) {
-  return Object.entries(entries).reduce(
-    (resultArray: any, [key, value], index) => {
-      const chunkIndex = Math.floor(index / chunkSize);
-
-      if (!resultArray[chunkIndex]) {
-        resultArray[chunkIndex] = {};
-      }
-
-      resultArray[chunkIndex][key] = value;
-
-      return resultArray;
-    },
-    []
-  );
+function chunkEntries(entries, chunkSize) {
+    return Object.entries(entries).reduce(function (resultArray, _a, index) {
+        var key = _a[0], value = _a[1];
+        var chunkIndex = Math.floor(index / chunkSize);
+        if (!resultArray[chunkIndex]) {
+            resultArray[chunkIndex] = {};
+        }
+        resultArray[chunkIndex][key] = value;
+        return resultArray;
+    }, []);
 }
-
-const groupedEntries = chunkEntries(elementEntries, 3);
-
-function createConfigForGroup(entries: any) {
-  return defineConfig({
-    name: "Build Elements Group",
-    clean: false,
-    dts: true,
-    target: "es2019",
-    format: ["cjs", "esm"],
-    entry: entries
-  });
+var groupedEntries = chunkEntries(elementEntries, 3);
+function createConfigForGroup(entries) {
+    return (0, tsup_1.defineConfig)({
+        name: "Build Elements Group",
+        clean: false,
+        dts: true,
+        target: "es2019",
+        format: ["cjs", "esm"],
+        entry: entries
+    });
 }
-
-export const groupedConfigs = groupedEntries.map((group: any) =>
-  createConfigForGroup(group)
-);
+exports.groupedConfigs = groupedEntries.map(function (group) {
+    return createConfigForGroup(group);
+});
