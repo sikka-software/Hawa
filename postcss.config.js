df0ba1316740da940752c40a8c71fd065ba4d8cb
zaaakher <zakhermasri@gmail.com>
2023-12-30 13:52:41 +0300

    Updating package.json and adding setting up pnpm

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,1 @@
-module.exports = {
-  plugins: {
-    "postcss-import": {},
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
+module.exports = {  plugins: {"postcss-import": {},tailwindcss: {},autoprefixer: {} }};
