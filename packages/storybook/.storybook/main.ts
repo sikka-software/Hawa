ca705453603141e44659902c209ca86c815ab049
zaaakher <zakhermasri@gmail.com>
2024-01-01 23:37:06 +0300

    Fixed styling in /storybook except for color configurtations from /components/tailwind.config.ts

--- packages/storybook/.storybook/main.ts
+++ packages/storybook/.storybook/main.ts
@@ -8,29 +8,40 @@ const config: StorybookConfig = {
     "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   addons: [
-    // {
-    //   name: "@storybook/addon-styling-webpack",
-    //   options: {
-    //     rules: [
-    //       // Replaces existing CSS rules to support PostCSS
-    //       {
-    //         test: /\.css$/,
-    //         use: [
-    //           "style-loader",
-    //           {
-    //             loader: "css-loader",
-    //             options: { importLoaders: 1 }
-    //           },
-    //           {
-    //             // Gets options from `postcss.config.js` in your project root
-    //             loader: "postcss-loader",
-    //             options: { implementation: require.resolve("postcss") }
-    //           }
-    //         ]
-    //       }
-    //     ]
-    //   }
-    // },
+    {
+      name: "@storybook/addon-styling-webpack",
+      options: {
+        rules: [
+          {
+            test: /\.css$/,
+            use: [
+              "style-loader",
+              {
+                loader: "css-loader",
+                options: { importLoaders: 1 }
+              },
+              {
+                loader: "postcss-loader",
+                options: {
+                  postcssOptions: {
+                    plugins: [
+                      require("tailwindcss")(
+                        path.resolve(
+                          __dirname,
+                          "../../components/tailwind.config.ts"
+                        )
+                      ),
+                      require("autoprefixer")
+                    ]
+                  },
+                  implementation: require.resolve("postcss")
+                }
+              }
+            ]
+          }
+        ]
+      }
+    },
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
@@ -46,32 +57,11 @@ const config: StorybookConfig = {
       ...(config.resolve.plugins || []),
       new TsconfigPathsPlugin({ extensions: config.resolve.extensions })
     ];
-    config.module.rules.push({
-      test: /\.css$/,
-      use: [
-        "style-loader",
-        "css-loader",
-        {
-          loader: "postcss-loader",
-          options: {
-            postcssOptions: {
-              plugins: [
-                require("tailwindcss")(
-                  path.resolve(__dirname, "../../components/tailwind.config.ts")
-                ),
-                require("autoprefixer")
-              ]
-            }
-          }
-        }
-      ],
-      include: path.resolve(__dirname, "../../components")
-    });
+
     config.resolve.alias = {
       ...config.resolve.alias,
       "@elements": path.resolve(__dirname, "../../components/elements"),
       "@blocks": path.resolve(__dirname, "../../components/blocks")
-      // Add other aliases similarly...
     };
 
     return config;
