7b0d0ae042d44c6a14dce446d0d213f34a29511b
zaaakher <zakhermasri@gmail.com>
2024-01-10 08:50:13 +0300

    chore(docs): updated footer

--- apps/www/next.config.mjs
+++ apps/www/next.config.mjs
@@ -1,55 +1,59 @@
-import { createContentlayerPlugin } from "next-contentlayer"
+import { createContentlayerPlugin } from "next-contentlayer";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["avatars.githubusercontent.com", "images.unsplash.com"],
+    domains: [
+      "avatars.githubusercontent.com",
+      "images.unsplash.com",
+      "sikka-images.s3.ap-southeast-1.amazonaws.com"
+    ]
   },
   redirects() {
     return [
       {
         source: "/components",
         destination: "/docs/components/accordion",
-        permanent: true,
+        permanent: true
       },
       {
         source: "/docs/components",
         destination: "/docs/components/accordion",
-        permanent: true,
+        permanent: true
       },
       {
         source: "/examples",
         destination: "/examples/mail",
-        permanent: false,
+        permanent: false
       },
       {
         source: "/docs/primitives/:path*",
         destination: "/docs/components/:path*",
-        permanent: true,
+        permanent: true
       },
       {
         source: "/figma",
         destination: "/docs/figma",
-        permanent: true,
+        permanent: true
       },
       {
         source: "/docs/forms",
         destination: "/docs/components/form",
-        permanent: false,
+        permanent: false
       },
       {
         source: "/docs/forms/react-hook-form",
         destination: "/docs/components/form",
-        permanent: false,
-      },
-    ]
-  },
-}
+        permanent: false
+      }
+    ];
+  }
+};
 
 const withContentlayer = createContentlayerPlugin({
   // Additional Contentlayer config options
-})
+});
 
-export default withContentlayer(nextConfig)
+export default withContentlayer(nextConfig);
