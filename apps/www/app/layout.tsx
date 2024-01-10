1d9a8c91181980330df8e740a87df5d81ac67f56
zaaakher <zakhermasri@gmail.com>
2024-01-10 07:34:33 +0300

    chore(docs): added IBM Plex Sans font

--- apps/www/app/layout.tsx
+++ apps/www/app/layout.tsx
@@ -1,25 +1,26 @@
-import "@/styles/globals.css"
+// sort-imports-ignore
+import "@/styles/globals.css";
 import "@sikka/hawa/dist/style.css";
 
-import { Metadata, Viewport } from "next"
+import { Metadata, Viewport } from "next";
 
-import { siteConfig } from "@/config/site"
-import { fontSans } from "@/lib/fonts"
-import { cn } from "@/lib/utils"
-import { Analytics } from "@/components/analytics"
-import { ThemeProvider } from "@/components/providers"
-import { SiteFooter } from "@/components/site-footer"
-import { SiteHeader } from "@/components/site-header"
-import { TailwindIndicator } from "@/components/tailwind-indicator"
-import { ThemeSwitcher } from "@/components/theme-switcher"
-import { Toaster as DefaultToaster } from "@/registry/default/ui/toaster"
-import { Toaster as NewYorkSonner } from "@/registry/new-york/ui/sonner"
-import { Toaster as NewYorkToaster } from "@/registry/new-york/ui/toaster"
+import { siteConfig } from "@/config/site";
+import { fontSans, IBMFont } from "@/lib/fonts";
+import { cn } from "@/lib/utils";
+import { Analytics } from "@/components/analytics";
+import { ThemeProvider } from "@/components/providers";
+import { SiteFooter } from "@/components/site-footer";
+import { SiteHeader } from "@/components/site-header";
+import { TailwindIndicator } from "@/components/tailwind-indicator";
+import { ThemeSwitcher } from "@/components/theme-switcher";
+import { Toaster as DefaultToaster } from "@/registry/default/ui/toaster";
+import { Toaster as NewYorkSonner } from "@/registry/new-york/ui/sonner";
+import { Toaster as NewYorkToaster } from "@/registry/new-york/ui/toaster";
 
 export const metadata: Metadata = {
   title: {
     default: siteConfig.name,
-    template: `%s - ${siteConfig.name}`,
+    template: `%s - ${siteConfig.name}`
   },
   metadataBase: new URL(siteConfig.url),
   description: siteConfig.description,
@@ -28,13 +29,13 @@ export const metadata: Metadata = {
     "React",
     "Tailwind CSS",
     "Server Components",
-    "Radix UI",
+    "Radix UI"
   ],
   authors: [
     {
       name: "shadcn",
-      url: "https://shadcn.com",
-    },
+      url: "https://shadcn.com"
+    }
   ],
   creator: "shadcn",
   openGraph: {
@@ -49,34 +50,34 @@ export const metadata: Metadata = {
         url: siteConfig.ogImage,
         width: 1200,
         height: 630,
-        alt: siteConfig.name,
-      },
-    ],
+        alt: siteConfig.name
+      }
+    ]
   },
   twitter: {
     card: "summary_large_image",
     title: siteConfig.name,
     description: siteConfig.description,
     images: [siteConfig.ogImage],
-    creator: "@shadcn",
+    creator: "@shadcn"
   },
   icons: {
     icon: "/favicon.ico",
     shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
+    apple: "/apple-touch-icon.png"
   },
-  manifest: `${siteConfig.url}/site.webmanifest`,
-}
+  manifest: `${siteConfig.url}/site.webmanifest`
+};
 
 export const viewport: Viewport = {
   themeColor: [
     { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
-}
+    { media: "(prefers-color-scheme: dark)", color: "black" }
+  ]
+};
 
 interface RootLayoutProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
@@ -86,8 +87,8 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <head />
         <body
           className={cn(
-            "min-h-screen bg-background font-sans antialiased",
-            fontSans.className
+            "bg-background min-h-screen font-sans antialiased",
+            IBMFont.className
           )}
         >
           <ThemeProvider
@@ -97,7 +98,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
             disableTransitionOnChange
           >
             <div vaul-drawer-wrapper="">
-              <div className="relative flex min-h-screen flex-col bg-background">
+              <div className="bg-background relative flex min-h-screen flex-col">
                 <SiteHeader />
                 <main className="flex-1">{children}</main>
                 <SiteFooter />
@@ -113,5 +114,5 @@ export default function RootLayout({ children }: RootLayoutProps) {
         </body>
       </html>
     </>
-  )
+  );
 }
