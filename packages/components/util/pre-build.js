1218b1cce7d7003736450a5af5356fda68a7a63d
zaaakher <zakhermasri@gmail.com>
2024-01-12 08:17:40 +0300

    updated build scripts

--- packages/components/util/pre-build.js
+++ packages/components/util/pre-build.js
@@ -1,13 +1,13 @@
 const fs = require("fs");
 const path = require("path");
 
-const distPath = path.join(__dirname, "dist");
+const distPath = path.join(__dirname, "..", "dist");
 
 // This function checks if the dist directory exists and removes it
 function cleanDistDirectory() {
   console.log("dist path is ", distPath);
   if (fs.existsSync(distPath)) {
-    fs.rmdirSync(distPath, { recursive: true });
+    fs.rmSync(distPath, { recursive: true });
     console.log("Removed dist directory.");
   } else {
     console.log("No dist directory to remove.");
