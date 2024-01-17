0505213834db4e562cf15a97ae68f8f112617c14
zaaakher <zakhermasri@gmail.com>
2024-01-17 07:48:40 +0300

    chore(docs): updated pre and code components in mdx

--- apps/docs/components/mdx-components.tsx
+++ apps/docs/components/mdx-components.tsx
@@ -193,82 +193,17 @@ const components = {
       {...props}
     />
   ),
-  // pre: ({
-  //   className,
-  //   __rawString__,
-  //   __npmCommand__,
-  //   __yarnCommand__,
-  //   __pnpmCommand__,
-  //   __bunCommand__,
-  //   __withMeta__,
-  //   __src__,
-  //   __event__,
-  //   __style__,
-  //   ...props
-  // }: React.HTMLAttributes<HTMLPreElement> & {
-  //   __style__?: Style["name"];
-  //   __rawString__?: string;
-  //   __withMeta__?: boolean;
-  //   __src__?: string;
-  //   __event__?: Event["name"];
-  // } & NpmCommands) => {
-  //   return (
-  //     <StyleWrapper styleName={__style__}>
-  //       <pre
-  //         className={cn(
-  //           "mb-4 mt-6 max-h-[650px] overflow-x-auto rounded-lg border bg-zinc-950 py-4 dark:bg-zinc-900",
-  //           className
-  //         )}
-  //         {...props}
-  //       />
-  //       {__rawString__ && !__npmCommand__ && (
-  //         <CopyButton
-  //           value={__rawString__}
-  //           src={__src__}
-  //           event={__event__}
-  //           className={cn("absolute right-4 top-4", __withMeta__ && "top-16")}
-  //         />
-  //       )}
-  //       {__npmCommand__ &&
-  //         __yarnCommand__ &&
-  //         __pnpmCommand__ &&
-  //         __bunCommand__ && (
-  //           <CopyNpmCommandButton
-  //             commands={{
-  //               __npmCommand__,
-  //               __yarnCommand__,
-  //               __pnpmCommand__,
-  //               __bunCommand__
-  //             }}
-  //             className={cn("absolute right-4 top-4", __withMeta__ && "top-16")}
-  //           />
-  //         )}
-  //     </StyleWrapper>
-  //   );
-  // },
   pre: ({
     className,
     children,
     ...props
   }: React.HTMLAttributes<HTMLPreElement>) => {
     const codeString = getCodeString(children);
-    return (
-      <CodeBlock
-        language="jsx"
-        code={codeString}
-        className={className}
-        {...props}
-      />
-    );
+    console.log("🚀🚀🚀 pre is ", props);
+    return <CodeBlock language="jsx" code={codeString} {...props} />;
   },
-  code: ({ className, ...props }: React.HTMLAttributes<HTMLElement>) => (
-    <code
-      className={cn(
-        "bg-muted relative rounded px-[0.3rem] py-[0.2rem] font-mono text-sm",
-        className
-      )}
-      {...props}
-    />
+  code: ({ ...props }: React.HTMLAttributes<HTMLElement>) => (
+    <span className="inline-code">{props.children}</span>
   ),
   Image,
   Callout,
