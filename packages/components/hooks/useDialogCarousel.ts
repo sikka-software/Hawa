85b27d0e2d53c6f6bc6673c0ce26f754858a5354
zakher <zakhermasri@gmail.com>
2024-06-19 22:43:35 +0300

    chore: remove `destroyHeight` auto height property from carousel

--- packages/components/hooks/useDialogCarousel.ts
+++ packages/components/hooks/useDialogCarousel.ts
@@ -14,12 +14,7 @@ type DialogCarouselType = {
 export const useDialogCarousel = (options?: any): DialogCarouselType => {
   const [emblaRef, emblaApi] = useEmblaCarousel(
     { loop: false, watchDrag: false, startIndex: 0, ...options },
-    [
-      AutoHeight({
-        // destroyHeight: "fit", //TODO: see what replaced this
-        active: true,
-      }),
-    ],
+    [AutoHeight({ active: true })],
   );
   const [canScrollPrev, setCanScrollPrev] = useState(false);
   const checkCanScrollPrev = () => {
